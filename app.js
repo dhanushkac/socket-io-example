49e08f1d5bc2a747a20649c8a7fe98dbeb2910e2
Dhanushka C <dhanushkac@thinkcube.com>
2017-01-04 15:52:16 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + 'bower_components'));
+app.get('/', function(req, res, next) {
+  res.sendFile(__dirname + '/index.html');
+});
+console.log('Server is running..');
+server.listen(4200);
+
+io.on('connection', function(client) {  
+  console.log('Client connected...');
+
+  client.on('join', function(data) {
+    console.log(data);
+    client.emit('messages', 'Hello from server');
+  });
+
+  client.on('messages', function(data) {
+    client.emit('broad', data);
+    client.broadcast.emit('broad',data);
+  });
+});
